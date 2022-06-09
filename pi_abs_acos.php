fadb4195fc1afd174e165405ea9b4ae5bb7f1037
jahangiralam7864 <wedevs95@gmail.com>
2022-06-10 00:35:42 +0600

    pi_

---
+++ pi_abs_acos.php
@@ -0,0 +1,21 @@
+<?php
+    // pi er man check 
+    echo pi();
+    // result: 3.1415926535898
+    echo "<br>";
+
+    // kono negative k positive kore abs
+    echo abs(-10);
+    // result: 10;
+
+    // Min / Max ar kisu kaj nice dewa holo
+    echo "<br>";
+    echo min(11,4,23);
+
+    echo "<br>";
+    $numbers = 20;
+    $num = 10;
+    $min = 5;
+    echo min(10,20,5);
+
+?>
