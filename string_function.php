b2869b9cb133c521d98970f0dc83aaae706ca836
jahangiralam7864 <wedevs95@gmail.com>
2022-06-14 02:24:46 +0600

    string

--- string_function.php
+++ string_function.php
@@ -5,7 +5,27 @@
     echo "<br>";
     // result : Jahangir alam joy jahangir
 
+    $names = "jahanigr alam monirul rafi uddin";
+    echo ucwords($names);
+    echo "<br>";
+    // result : Jahanigr Alam Monirul Rafi Uddin
 
+    $name = "OUR PHP CLASS NO 8";
+    echo lcfirst($name);
+    echo "<br>";
+    // result: oUR PHP CLASS NO 8
+
+    $name = "OUR PHP CLASS NO 8";
+    echo strtolower($name);
+    echo "<br>";
+    // result: our php class no 8
+
+    $name = "OUR PHP CLASS NO 8";
+    $name = strtolower($name);
+    $name = str_replace(" ", "_", $name);
+    echo "$name";
+    echo "<br>";
+    // result: our_php_class_no_8
 
 
 ?>
