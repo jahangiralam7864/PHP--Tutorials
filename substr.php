0749d548635ac77d33e774ca7d0c86c6a0787f75
jahangiralam7864 <wedevs95@gmail.com>
2022-06-16 09:57:06 +0600

    strstr

--- substr.php
+++ substr.php
@@ -2,9 +2,23 @@
 
     $name = "amar sonar bangla";
     echo substr($name, 0, 5);
+    // result:amar
     echo "<br>";
     var_dump(substr($name, 0, 5));
+    // result:string(5) "amar "
     echo "<br>";
+    
+    $myName = "jahangir";
+    var_dump(strstr($myName, "alam"));
+    echo "<br>";
+    
+    if (! strstr($myName, "rafi uddin")) {
+        echo "Sorry, Name doesn't match";
+    }else {
+        echo " You are right now";
+    }
+
+    
 
 
 
