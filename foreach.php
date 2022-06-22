ab34766975892d28e844f92bf4c9ee997db15961
jahangiralam7864 <wedevs95@gmail.com>
2022-06-23 02:51:05 +0600

    foreach

---
+++ foreach.php
@@ -0,0 +1,17 @@
+<?php
+
+$names = [
+    "Md jahangir alam",
+    "Md Rafi Uddin",
+    "Md Abdul Hamid",
+    "Md Tariqul Islam",
+    "Md shofiqul Islam"
+];
+?>
+<ol>
+<?php
+    foreach ($names as $parson) {
+        echo "<li><br>" . $parson . "</br></li>";
+    }
+?>
+</ol>
