16a66667c995b8d37808d2a4c9aefeffcbe4be8c
jahangiralam7864 <wedevs95@gmail.com>
2022-06-23 01:50:57 +0600

    Odd

--- loops.php
+++ loops.php
@@ -5,3 +5,15 @@
     }
     echo "Out site";
     // result: 1-9 pojonto 10 ar soto.
+
+    // Even Number Program
+    for ($i=0; $i < 20; $i += 2) { 
+        echo $i. "<br>";
+    } 
+
+    // Odd Number Program
+    for ($x=1; $x < 10; $x += 2) { 
+        if ($x % 1 == 0) {
+            echo $x . "<br>";
+        }
+    }
