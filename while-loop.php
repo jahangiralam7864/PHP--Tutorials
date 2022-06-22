6a807921c0769999f994c82e8e9cebcaa24da746
jahangiralam7864 <wedevs95@gmail.com>
2022-06-23 02:09:06 +0600

    loop

---
+++ while-loop.php
@@ -0,0 +1,18 @@
+<?php
+// Even Number 
+$x = 0;
+while ($x < 20) {
+    if ($x % 2 == 0) {
+        echo $x . "<br>";
+    }
+    $x++;
+}
+
+// Odd number
+$x = 0;
+while ($x < 20) {
+    if ($x % 2 == 1) {
+        echo $x . "<br>";
+    }
+    $x++;
+}
