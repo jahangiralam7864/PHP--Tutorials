e3b48018560d99040275093253e3183a9c5c9b96
jahangiralam7864 <wedevs95@gmail.com>
2022-08-12 07:21:07 +0600

    push

--- class1.php
+++ class1.php
@@ -7,36 +7,11 @@
     <title>Document</title>
 </head>
 <body>
-    <?php
-        // echo "Welcome to learn php";
-        // print "Welcome to learn php";
+    <script>
+        let myName = "Jahangir";
+        let myName = "alam";
+        console.log(myName);
 
-        $number1 = 10;
-        $number2 = 20;
-        echo $number1;
-        echo $number2 . "<br>";
-        $sum = $number1 + $number2;
-        $sub = $number2 - $number1;
-
-        echo "<strong>sum<strong> = " . $sum . "<br>";
-        echo "<strong>sub<strong> = " . $sub . "<br>";
-        // print total = $sum + $sub;
-
-        $numb1 = 20;
-        $summation = $numb1 + 30 + 50 -20;
-        echo "Total Amount = ". $summation . "<br>";
-
-        $number1 = 10;
-        $number2 = 20;
-    echo $number1 + $number2 . "<br>";
-    // Result 30
-    
-    $x = 20;
-    echo "Increment = " . ++$x;
-  
-    ?>
-
-
-
+    </script>
 </body>
 </html>
